6c95848f029b43a9480a7670a61e703898da915a
Alexander Bock <abock@ipp.mpg.de>
2015-08-28 14:10:06 +0200

    massive rewrite of A-factor calculation: now uses pure vector approach with a measurement surface perpendicular to each LOS which should result in a single offset for all channels, provided that the measurement geometry is well-known also now using sprd's LOS data rather than making our own from FARO

--- lib/RzAmaker.py
+++ lib/RzAmaker.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-#import os, sys
+import sys
 import numpy as np
 #import dd_20140409 as dd, ww_20140403 as ww
 import kk_abock as kk
-#import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt; plt.ion()
 #import matplotlib as mpl
 #import argparse
 #from pylab import specgram
@@ -14,41 +14,88 @@ import kk_abock as kk
 from IPython import embed
 #from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import cdist
-from copy import copy
 
-sin, cos = np.sin, np.cos
+sin, cos, norm, dot, cross = np.sin, np.cos, np.linalg.norm, np.dot, np.cross
 
 class Bunch(object):
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
 
+def getLOSfromSPRD(year):
+    lines = [l for l in open('/afs/ipp/u/sprd/loscoord/LOS_COORD_%4i'%year).readlines() if 'MSS' in l]
+    s0 = np.zeros((60,3))
+    s1 = np.zeros((60,3))
+    for i, l in enumerate(lines):
+        R0, phi0, z0, R1, phi1, z1 = np.array(l.split()[1:],float)
+        s0[i] = R0*cos(phi0/180.*np.pi), R0*sin(phi0/180.*np.pi), z0
+        s1[i] = R1*cos(phi1/180.*np.pi), R1*sin(phi1/180.*np.pi), z1
+    ds = s1-s0; ds = (ds.T/norm(ds, axis=1)).T
+    return s0, ds
 
-def makeRzAs(mseFaroFile='mse2014.txt', plot=False, beamSubdivisionLength=1e-3):
+def getNBIgeo():
+    ''' NBI geometry from bgeiger '''
+    src = np.array([6.1870004,   -6.8164635,  -0.69000000])
+    v = np.array([-1401832.2, 1904877., 229791.33]) 
+    v /= norm(v)
+    return src+7*v, v # don't need full beam length as we only care for MSE LOS, so move closer
+
+def getApproxIntersections(s0, s1, v0, v1, beamSubdivisionLength):
+    ''' determine MSE LsOS interections with NBI '''
+    sn = norm(s1 - s0, axis=1)/beamSubdivisionLength
+    vn = norm(v1 - v0)/beamSubdivisionLength
+    vmat = np.zeros((vn, 3))
+    for j in xrange(3):
+        vmat[:,j] = np.linspace(v0[j], v1[j], vn)
+    intersections = np.zeros_like(s0)
+    for i in xrange(s0.shape[0]):
+        smat = np.zeros((sn[i], 3))
+        for j in xrange(3):
+            smat[:,j] = np.linspace(s0[i,j], s1[i,j], sn[i])
+        dmat = cdist(smat, vmat, 'euclidean') # calculates n*bn matrix of all distances
+        s_ind, v_ind = np.unravel_index(dmat.argmin(), dmat.shape)
+        #cv = vmat[v_ind]
+        cs = smat[s_ind]
+        intersections[i] = cs #(cv+cs)/2.
+    return intersections
+
+def getMSExperp(s0, s):
+    ''' determine MSE switchboard x-vector in torus space from MSE LsOS '''
+    s1 = s0+2*s
+    p0 = np.average(s0, axis=0)
+    p1 = np.average(s1, axis=0)
+    dp = np.average(s, axis=0)
+    dp = dp/norm(dp)
+
+    mps = []
+    for i in xrange(s0.shape[0]):
+        d = dot((p1-s0[i]), dp) / dot(s[i], dp)
+        mp = d*s[i] + s0[i]
+        mps.append(mp)
+    mps = np.array(mps)
+
+    vecs = []
+    for offset in range(6):
+        for i, m in enumerate(mps[offset::6]):
+            if i+1 > 9: break
+            n = mps[offset::6][i+1]
+            vecs.append((n-m)/norm(n-m))
+    vecs = np.array(vecs)
+    vec = np.average(vecs, axis=0)
+    vec /= norm(vec)
+    return vec
+
+def makeRzAs(year=2014, plot=False, beamSubdivisionLength=1e-3, channels=range(60)):
     '''generates MSE R, z, A coords for pi and sigma from a FARO measurement'''
-    # shorter = faster, too short = wrong result:
-    # check with plot option above
-    nbiBeamLength = 1.2
-    mseBeamLength = 2. 
 
-    btres = beamSubdivisionLength # beam point interval in m
+    s0, s = getLOSfromSPRD(int(year))
 
-    # from David Rittich/Felician Mink/Christian Hopf
-    Q3 = np.array([[9.248975, 20.3955, -0.600], # R, phi (0 south), z
-                   [2.37409,  37.7203,  0]])
+    s1 = s0 + 2.2*s
+    s0 = s0 + 1.0*s
 
-    Q3[:,1] = Q3[:,1]/180.*np.pi # rotate into common coordinate system
+    v0, v = getNBIgeo()
+    v1 = v0 + v
 
-    Q3c = np.zeros_like(Q3) # create cartesian coordinate array
-    # fill it...
-    Q3c[:,0] = Q3[:, 0]*cos(Q3[:,1])
-    Q3c[:,1] = Q3[:, 0]*sin(Q3[:,1])
-    Q3c[:,2] = Q3[:,2]
-    vdx = Q3c[1,0] - Q3c[0,0]
-    vdy = Q3c[1,1] - Q3c[0,1]
-    vdz = Q3c[1,2] - Q3c[0,2]
-    vd = (vdx**2+vdy**2+vdz**2)**0.5 / nbiBeamLength
-    # use original inner point and another point inside plasma
-    Q3c[:] = Q3c[1], Q3c[1] + np.array([vdx/vd, vdy/vd, vdz/vd])
+    isecs = getApproxIntersections(s0, s1, v0, v1, beamSubdivisionLength)
 
     if plot:
         try:
@@ -56,172 +103,144 @@ def makeRzAs(mseFaroFile='mse2014.txt', plot=False, beamSubdivisionLength=1e-3):
         except Exception, e:
             print 'mayavi failed to load, are you using intel/12.1?'
             raise e
-        mlab.plot3d(Q3c[:,0], Q3c[:,1], Q3c[:,2], color=(0,1,0))
+        for i in xrange(60):
+            ts0 = s0 - 1.5*s
+            mlab.plot3d([ts0[i,0], s1[i,0]], [ts0[i,1], s1[i,1]], [ts0[i,2], s1[i,2]], tube_radius=None, color=(1,0,0))
+        mlab.points3d(isecs[:,0], isecs[:,1], isecs[:,2])
+        mlab.plot3d([v0[0], v1[0]], [v0[1], v1[1]], [v0[2], v1[2]], color=(0,1,0))
+        mlab.view(0, 0, distance=10, focalpoint=[0,0,0])
+
+        for R in [1.65]: #, 1.8, 1.95]: # axis
+            x = R * cos(np.linspace(0,2*np.pi, 128))
+            y = R * sin(np.linspace(0,2*np.pi, 128))
+            mlab.plot3d(x,y,[0]*len(x), tube_radius=None)
+
         eq = kk.kk()
         eq.Open(31113, 'AUGD', 'EQI')
         sepR, sepz = [], []
         for a in xrange(360):
             res = eq.rhopol_to_Rz(3.0, 0.99, a, True)
-            #print res
-            sepR.append(res['R'])# [0])
-            sepz.append(res['z'])# [0])
+            sepR.append(res['R'])
+            sepz.append(res['z'])
         sepR = np.array(sepR)
         sepz = np.array(sepz)
-        mlab.plot3d(np.cos(Q3[1,1])*sepR, np.sin(Q3[1,1])*sepR, sepz, tube_radius=None)
+        # separatrix:
+        phi = -0.37
+        mlab.plot3d(np.cos(phi)*sepR, np.sin(phi)*sepR, sepz, tube_radius=None)
 
 
-    # load MSE coords from Faro
-    msecoords = np.loadtxt(mseFaroFile, delimiter=';', usecols=(1,2,3))/1e3
+    xperp = getMSExperp(s0, s)
 
-    # buffer for better suitable coords
-    nmc = np.zeros_like(msecoords) # new mse coords
-    # fill it...
-    for i in xrange(60):
-        x = msecoords[[i,i+60], 0]
-        y = msecoords[[i,i+60], 1]
-        z = msecoords[[i,i+60], 2]
-        # rotate into common coordinate system...
-        R = (x**2 + y**2)**0.5
-        phi = np.arctan2(y, x) + 6*np.pi/16.
-        x = R*cos(phi)
-        y = R*sin(phi)
-        dx = x[1] - x[0]
-        dy = y[1] - y[0]
-        dz = z[1] - z[0]
-        d = (dx**2+dy**2+dz**2)**0.5 / mseBeamLength
+    isecR = (isecs[:,0]**2 + isecs[:,1]**2)**0.5
+    isecZ = isecs[:,2]
+    isecPhi = np.arctan2(isecs[:,1], isecs[:,0]) 
 
-        sp = 1
-        nmc[i,:] = x[sp]-dx/d*2., y[sp]-dy/d*2., z[sp]-dz/d*2.
-        nmc[i+60,:] = x[sp]+dx/d, y[sp]+dy/d, z[sp]+dz/d
-        if plot:
-            mlab.plot3d(nmc[[i,i+60],0],nmc[[i,i+60],1],nmc[[i,i+60],2], line_width=1.,
-                tube_radius=None, color=(1,0,0))
-        
+    eR = np.zeros_like(isecs)
+    eR[:,0], eR[:,1] = cos(isecPhi), sin(isecPhi)
+    eT = np.zeros_like(isecs)
+    eT[:,0], eT[:,1] =  -sin(isecPhi), cos(isecPhi)
+    eZ = np.zeros_like(isecs)
+    eZ[:,2] = 1.
 
-    # plot magnetic axis for reference
-    if plot:
-        x = 1.65 * cos(np.linspace(0,2*np.pi, 128))
-        y = 1.65 * sin(np.linspace(0,2*np.pi, 128))
-        mlab.plot3d(x,y,[0]*len(x))
-        mlab.view(0, 0, distance=10, focalpoint=[0,0,0])
+    sR = np.sum(s * eR, axis=1)
+    st = np.sum(s * eT, axis=1)
+    sz = np.sum(s * eZ, axis=1)
 
-    # calculate intersections and local A's:
+    xpR = np.sum(xperp * eR, axis=1)
+    xpt = np.sum(xperp * eT, axis=1)
+    xpz = np.sum(xperp * eZ, axis=1)
 
-    # delta: between v and x,y plane, i.e. independent of MSE
-    vxy = (vdx**2 + vdy**2)**0.5
-    vd = (vdx**2+vdy**2+vdz**2)**0.5
-    delta = np.arccos(vxy/vd)
+    vR = np.sum(v * eR, axis=1)
+    vt = np.sum(v * eT, axis=1)
+    vz = np.sum(v * eZ, axis=1)
 
-    # 1st: generate beam points:
-    bn = nbiBeamLength / btres
-    bx = np.linspace(Q3c[0,0], Q3c[1,0], bn)
-    by = np.linspace(Q3c[0,1], Q3c[1,1], bn)
-    bz = np.linspace(Q3c[0,2], Q3c[1,2], bn)
-    # make a matrix from points
-    bmat = np.zeros((bn, 3))
-    bmat[:,0], bmat[:,1], bmat[:,2] = bx, by, bz
+    A1  = -st*vt*xpR - sz*vz*xpR + sR*vt*xpt + sR*vz*xpz
+    A2  = -(sR*vR + sz*vz)*xpt + st*(vR*xpR+vz*xpz)
+    A3  = sz*(vR*xpR + vt*xpt) - (sR*vR + st*vt)*xpz
+    A4  = sz*xpt - st*xpz
+    A5  = st*xpR - sR*xpt
+    A6  = sR*(sz*vt - st*vz)*xpR - (st*vt + sz*vz)*(-sz*xpt + st*xpz) + (sR**2)*(vz*xpt - vt*xpz)
+    A7  = -sz**2*vz*xpR - st*sz*vR*xpt + sR**2*vR*xpz + st**2*(-vz*xpR + vR*xpz) + sR*(-sz*vR*xpR + st*vz*xpt + sz*vz*xpz)
+    A8  = st**2*vt*xpR - sR**2*vR*xpt + sz**2*(vt*xpR - vR*xpt) + st*sz*vR*xpz + sR*(st*vR*xpR - st*vt*xpt - sz*vt*xpz)
+    A9  = (st**2*xpR + sz**2*xpR - sR*st*xpt - sR*sz*xpz)
+    A10 = (-sR*sz*xpR + sR**2*xpz + st*(-sz*xpt + st*xpz))
 
-    # preliminary output buffers
-    R = np.zeros(60)
-    Z = np.zeros(60)
-    Asigma = np.zeros((60,10))
-    Api = np.zeros((60,10))
+    Asigma = np.array((A1, A2, A3, A4, A5, A6, A7, A8, A9, A10)).T
+    Api = np.array((A6, A7, A8, A9, A10, A1, A2, A3, A4, A5)).T
 
-    for i in xrange(60):
-        n = mseBeamLength / btres
-        x = np.linspace(nmc[i,0],nmc[i+60,0], n)
-        y = np.linspace(nmc[i,1],nmc[i+60,1], n)
-        z = np.linspace(nmc[i,2],nmc[i+60,2], n)
-        # make a matrix from points
-        pmat = np.zeros((n, 3))
-        pmat[:,0], pmat[:,1], pmat[:,2] = x, y, z
-
-        dmat = cdist(bmat, pmat, 'euclidean') # calculates n*bn matrix of all distances
-        # get indices of points closest to each other
-        beamindex, mseindex = np.unravel_index(dmat.argmin(), dmat.shape)
-
-        r, z = (x[mseindex]**2 + y[mseindex]**2)**0.5, z[mseindex] # R, z
-
-        R[i] = r 
-        Z[i] = z
-
-        # now: A1-10 for pi and sigma
-        # alpha: between (vr, vt, 0) and (sr, st, 0) (i.e. between v and s in x,y plane)
-        s = nmc[i+60,:]-nmc[i,:]
-        sxy = (s[0]**2+s[1]**2)**0.5
-        vz0 = np.array([vdx, vdy])
-        alpha = np.arccos(np.dot(vz0,s[:2])/vxy/sxy)
-        # theta: between s and x,y plane
-        sz0 = copy(s)
-        sz0[2] = 0 
-        theta = np.arccos(np.dot(s, sz0)/np.linalg.norm(sz0)/np.linalg.norm(s))
-        # Omega: between (sr, st, 0) and e_T
-        phi = np.arctan2(y[mseindex], x[mseindex])
-        eT = np.array([-sin(phi),cos(phi),0])
-        Omega = np.arccos(np.dot(sz0, eT)/np.linalg.norm(sz0))
-
-        # sigma
-        A1  =  sin(delta) * sin(Omega)
-        A2  = -cos(Omega) * sin(delta)
-        A3  =  cos(delta) * cos(alpha-Omega)
-        A4  =  cos(Omega)
-        A5  =  0
-        A6  =  (-cos(alpha) * cos(delta)) * cos(theta) - (cos(Omega) * sin(delta)) * sin(theta)
-        A7  =  (-cos(delta) * cos(theta)) * sin(alpha) - (sin(delta) * sin(theta)) * sin(Omega)
-        A8  =  (-cos(delta) * sin(theta)) * sin(alpha-Omega)
-        A9  =   (sin(theta) * sin(Omega))
-        A10 = cos(theta)
-        Asigma[i, :] = A1, A2, A3, A4, A5, A6, A7, A8, A9, A10
-
-        # pi
-        A1  =   (cos(alpha) * cos(delta)) * cos(theta) + (cos(Omega) * sin(delta)) * sin(theta)
-        A2  =   (cos(delta) * cos(theta)) * sin(alpha) + (sin(delta) * sin(theta)) * sin(Omega)
-        A3  =   (cos(delta) * sin(theta)) * sin(alpha-Omega)
-        A4  = -((sin(theta) * sin(Omega)))
-        A5  = -(cos(theta))
-        A6  =   sin(delta) * sin(Omega)
-        A7  =  -cos(Omega) * sin(delta)
-        A8  =   cos(delta) * cos(alpha-Omega)
-        A9  =   cos(Omega)
-        A10 = 0
-        Api[i, :] = A1, A2, A3, A4, A5, A6, A7, A8, A9, A10
-
-    # prepare final output buffers
     output = Bunch(
-                    R = np.zeros(60),
-                    z = np.zeros(60),
-                    Asigma = np.zeros((60,10)),
-                    Api = np.zeros((60,10))
-                  )
-    # map to our nomenclature, i.e. top right first channel, bottom left last channel
-    zOrder = Z.argsort() # get z order, then iterate from top row to bottom row:
+                R = np.zeros(60),
+                z = np.zeros(60),
+                Asigma = np.zeros((60,10)),
+                Api = np.zeros((60,10)),
+              )
+
+    # map to our nomenclature, i.e. top right on switchbard = first channel, bottom left last channel
+    # remember the directions are flipped in the torus, i.e. top line becomes bottom, rightmost column
+    # becomes outermost LOS, etc.
+    newOrder = []
+    order = isecZ.argsort()
     for i in xrange(5,-1,-1):
-        rs = R[zOrder[i*10:(i+1)*10]]
-        zs = Z[zOrder[i*10:(i+1)*10]]
-        asi = Asigma[zOrder[i*10:(i+1)*10]]
-        api = Api[zOrder[i*10:(i+1)*10]]
-        rOrder = rs.argsort() # get R order in current row, and apply it:
-        rs = rs[rOrder][::-1]
-        zs = zs[rOrder][::-1]
-        asi = asi[rOrder][::-1]
-        api = api[rOrder][::-1]
-        # put into output buffer
-        output.R[(-i+5)*10:(-i+6)*10] = rs
-        output.z[(-i+5)*10:(-i+6)*10] = zs
-        output.Asigma[(-i+5)*10:(-i+6)*10] = asi
-        output.Api[(-i+5)*10:(-i+6)*10] = api
+        o = order[10*i:10*(i+1)]
+        order2 = isecR[o].argsort()
+        #print o[order2]
+        newOrder += list(o[order2])
+    newOrder.reverse()
+
+    output.R = isecR[newOrder]
+    output.z = isecZ[newOrder]
+    output.Asigma = Asigma[newOrder]
+    output.Api = Api[newOrder]
 
     return output
 
 
 if __name__ == '__main__':
-    asd = makeRzAs(mseFaroFile='mse2015.txt', plot=True)
+    chans1 = [10, 11, 12, 13, 14, 15, 16, 17, 28, 39]
+    chans2 = [20, 21, 22, 23, 24, 25, 26, 27, 38, 49]
+    #chans1 = [1, 7, 13, 19, 25, 31, 37, 43, 50, 57]
+    #chans2 = [2, 8, 14, 20, 26, 32, 38, 44, 51, 58]
+    print 'new...'
+    asd = makeRzAs(2015, plot=False)
+
+    A = (asd.Asigma[chans1] + asd.Asigma[chans2]) / 2.
+    def gammas(Bt, Bz, Ez, A):
+        g_m = np.arctan2(A[:,1]*Bt + A[:,2]*Bz + A[:,4]*Ez, A[:,6]*Bt + A[:,7]*Bz + A[:,9]*Ez)
+        return g_m/np.pi*180
+
     embed()
+    sys.exit()
+
+    print 'old...'
+    from RzAmakerOld import makeRzAs as oldmrsa
+    asd2 = oldmrsa(mseFaroFile='mse2015.txt', plot=False)
+    #sys.exit()
+    Asigma = (asd.Asigma[chans1] + asd.Asigma[chans2]) / 2.
+    Asigma2 = (asd2.Asigma[chans1] + asd2.Asigma[chans2]) / 2.
+    Api = (asd.Api[chans1] + asd.Api[chans2]) / 2.
+    Api2 = (asd2.Api[chans1] + asd2.Api[chans2]) / 2.
+    Alist = [('newsig', Asigma), ('oldsig', Asigma2)] #, ('newpi ', Api), ('oldpi ', Api2)]
+    for i in xrange(10):
+        for name, As in Alist:
+            print '%s %2i '%(name, i+1),
+            for j in xrange(10):
+                print '% 5.4f '%As[i, j],
+            print 
+            break
+        
+
+
+    #embed()
 
 
 
+#             A1*Br + A2*Bt + A3*Bz + A4*Er/vb + A5*Ez/vb
+# tan g_m =  ---------------------------------------------
+#             A6*Br + A7*Bt + A8*Bz + A9*Er/vb + A10*Ez/vb
 
-
+#             A[0]*Br + A[1]*Bt + A[2]*Bz + A[3]*Er/vb + A[4]*Ez/vb
+# tan g_m =  ------------------------------------------------------
+#             A[5]*Br + A[6]*Bt + A[7]*Bz + A[8]*Er/vb + A[9]*Ez/vb
 
 
 
